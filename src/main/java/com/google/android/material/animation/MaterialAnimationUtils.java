c25a317c2015da622d5e982c89042d346307cfb1
Ashera Cordova <ashera.cordova@gmail.com>
2025-11-21 14:59:09 +0530

    Added licensing to all files + ratingbar support.

--- src/main/java/com/google/android/material/animation/MaterialAnimationUtils.java
+++ src/main/java/com/google/android/material/animation/MaterialAnimationUtils.java
@@ -1,3 +1,18 @@
+//start - license
+/*
+ * Copyright (c) 2025 Ashera Cordova
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+//end - license
 /*
  * Copyright (C) 2015 The Android Open Source Project
  *
@@ -42,17 +57,17 @@ public class MaterialAnimationUtils {
       new LinearOutSlowInInterpolator();
   public static final TimeInterpolator DECELERATE_INTERPOLATOR = new DecelerateInterpolator();
 
-  /** Linear interpolation between {@code startValue} and {@code endValue} by {@code fraction}. */
+ /** Linear interpolation between {@code startValue} and {@code endValue} by {@code fraction}. */
   public static float lerp(float startValue, float endValue, float fraction) {
     return startValue + (fraction * (endValue - startValue));
   }
 
-  /** Linear interpolation between {@code startValue} and {@code endValue} by {@code fraction}. */
+ /** Linear interpolation between {@code startValue} and {@code endValue} by {@code fraction}. */
   public static int lerp(int startValue, int endValue, float fraction) {
     return startValue + Math.round(fraction * (endValue - startValue));
   }
 
-  /**
+ /**
    * Linear interpolation between {@code outputMin} and {@code outputMax} when {@code value} is
    * between {@code inputMin} and {@code inputMax}.
    *
--- src/main/java/com/google/android/material/internal/MaterialCheckable.java
+++ src/main/java/com/google/android/material/internal/MaterialCheckable.java
@@ -1,3 +1,18 @@
+//start - license
+/*
+ * Copyright (c) 2025 Ashera Cordova
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+//end - license
 /*
  * Copyright 2022 The Android Open Source Project
  *
@@ -35,12 +50,12 @@ public interface MaterialCheckable<T extends MaterialCheckable<T>> extends Check
 
   void setInternalOnCheckedChangeListener(@Nullable OnCheckedChangeListener<T> listener);
 
-  /**
+ /**
    * Interface definition for a callback to be invoked when a {@link MaterialCheckable} is checked
    * or unchecked.
    */
   interface OnCheckedChangeListener<C> {
-    /**
+   /**
      * Called when the checked state of a {@link MaterialCheckable} has changed.
      *
      * @param checkable The compound button view whose state has changed.
